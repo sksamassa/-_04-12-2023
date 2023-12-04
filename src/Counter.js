cff0925c5069d0e7f653fdc54d57d7cc9b7b277f
Sekou Samassa <sksamassa@gmail.com>
2023-12-04 21:32:02 +0500

    Now we have a second button which decrement the counter if it is greater that null

--- src/Counter.js
+++ src/Counter.js
@@ -7,11 +7,24 @@ const Counter = () => {
     setCount((c) => c + 1);
   }
 
+  function handleDecrement(){
+    if(count > 0){
+        setCount(c => c - 1);
+    }
+  }
+
   return (
     <div className="Counter">
       <p className="App_counter">{count}</p>
       <button className="App__btn" onClick={handleIncrement}>
         +
+      </button>
+
+      <button 
+      className="App__btn" 
+      onClick={handleDecrement}
+      disabled={count <= 0 ? true : false}>
+        -
       </button>
     </div>
   );
