693f03f66ea18490f87ef996be80dd9573a74a9f
Sekou Samassa <sksamassa@gmail.com>
2023-12-04 22:55:11 +0500

    Let's persist the state with localStorage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Counter from './Counter';
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Counter initialValue={0} />
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+  // Retrieve stored data and set it as initial state
+  useEffect(() => {
+    const storedCount = JSON.parse(localStorage.getItem("count"));
+    if (storedCount) {
+      setCount(parseInt(storedCount, 10));
+    }
+  }, []);
+
+  // This ensures that the data is saved even if the user refreshes 
+  // the page or closes and reopens the browser
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
 
   function handleIncrement() {
     setCount((c) => c + 1);
   }
 
-  function handleDecrement(){
-    if(count > 0){
-        setCount(c => c - 1);
-    }
+  function handleDecrement() {
+    setCount(c => {
+        if(c > 0){
+            return c - 1;
+        } else {
+            return c;
+        }
+    })
   }
 
   return (
